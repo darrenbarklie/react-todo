783e8d8e25fe1b594aeace3824642d70d41fcee0
Darren Barklie <darrenbarklie@gmail.com>
2017-10-08 14:29:48 +0100

    Base styles

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">React ToDos</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Todo-App">
+          <form>
+            <input type="text" />
+          </form>
+          <div className="Todo-List">
+            <ul>
+              <li><input type="checkbox" />Learn JSX</li>
+              <li><input type="checkbox" />Build an app</li>
+              <li><input type="checkbox" />Ship it</li>
+            </ul>
+          </div>
+        </div>
       </div>
     );
   }
