65ae828ec3992b6a3e209e7aaf52e0a6d6e916f1
Darren Barklie <darrenbarklie@gmail.com>
2017-10-27 03:01:01 +0100

    New todo success message

--- src/App.js
+++ src/App.js
@@ -45,7 +45,12 @@ class App extends Component {
       errorMessage: ''
     })
     createTodo(newTodo)
-      .then(() => console.log('Todo added'))
+      .then(() => this.showTempMessage('Todo added'))
+  }
+
+  showTempMessage = (msg) => {
+    this.setState({message: msg})
+    setTimeout(() => this.setState({message: ''}), 2500)
   }
 
   handleEmptySubmit = (evt) => {
@@ -73,6 +78,7 @@ class App extends Component {
 
         <div className="Todo-App">
           {this.state.errorMessage && <span className="error">{this.state.errorMessage}</span>}
+          {this.state.message && <span className="success">{this.state.message}</span>}
 
           <TodoForm handleInputChange={this.handleInputChange}
             currentTodo={this.state.currentTodo}
